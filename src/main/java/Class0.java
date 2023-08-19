55272d6d3499ea4a43887d563b81cf5c99ce76bd
unknown <avktoguz@gmail.com>
2023-08-19 21:14:13 +0300

    v1

---
+++ src/main/java/Class0.java
@@ -0,0 +1,18 @@
+public class Class0 {
+    public static void main(String[] args) {
+
+
+        //versiyon1
+        int x=1;
+        int y=2;
+        System.out.println(x+y);
+
+
+
+
+
+
+
+
+    }
+}
